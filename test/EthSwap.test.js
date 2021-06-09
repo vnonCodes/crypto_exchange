13be61b0206ca662085b43a7313c68f82c0a3fe3
vnonCodes <engageanon@gmail.com>
2021-06-09 23:34:27 +0800

    Update EthSwap.test.js
    using web3 utility to use Wei against 6dp

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -5,13 +5,17 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
+function tokens(n) {
+    return web3.utils.toWei(n, 'ether');
+}
+
 contract( 'EthSwap', (accounts) => {
     let token, ethSwap
 
     before(async () => {
         token = await Token.new()
         ethSwap = await EthSwap.new()
-        await token.transfer(ethSwap.address, '1000000000000000000000000')
+        await token.transfer(ethSwap.address, tokens('1000000'))
     })
 
     describe('Token deployment', async () => {
@@ -29,7 +33,7 @@ contract( 'EthSwap', (accounts) => {
 
         it('contract has tokens', async () => {
             let balance = await token.balanceOf(ethSwap.address)
-            assert.equal(balance.toString(), '1000000000000000000000000')
+            assert.equal(balance.toString(), tokens('1000000'))
         })
     })
 
