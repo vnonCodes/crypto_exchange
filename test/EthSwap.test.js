b203b292c2229d653dbedeaa00dc347590181481
vnonCodes <engageanon@gmail.com>
2021-06-09 23:14:35 +0800

    Update EthSwap.test.js
    EthSwap contract has tokens, test passed

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -21,6 +21,14 @@ contract( 'EthSwap', (accounts) => {
             const name = await ethSwap.name()
             assert.equal(name, 'EthSwap Instant Exchange')
         })
+
+        it('contract has tokens', async () => {
+            let token = await Token.new()
+            let ethSwap = await EthSwap.new()
+            await token.transfer(ethSwap.address, '1000000000000000000000000')
+            let balance = await token.balanceOf(ethSwap.address)
+            assert.equal(balance.toString(), '1000000000000000000000000')
+        })
     })
 
 })
