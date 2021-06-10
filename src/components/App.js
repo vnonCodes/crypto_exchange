df92f0bf95cbece9fb25970838e2cc2549ae6355
vnonCodes <engageanon@gmail.com>
2021-06-10 16:49:09 +0800

    Updated navbar.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
+import Navbar from './Navbar'
 import './App.css'
 
 class App extends Component {
   
   async componentWillMount() {
     await this.loadWeb3()
-    console.log(window.web3)
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData () {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    console.log(this.state.account)
+
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ethBalance })
+    console.log(this.state.ethBalance)
   }
 
   async loadWeb3() {
@@ -22,19 +35,18 @@ class App extends Component {
     }
   }
   
+  constructor(props) {
+    super(props)
+    this.state = { 
+      account: '',
+      ethBalance: '0'
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
