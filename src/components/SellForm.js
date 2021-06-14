223f2d2317f3a625cde0e23e50ba35f82be313d9
vnonCodes <engageanon@gmail.com>
2021-06-14 11:27:50 +0800

    Updated form.js
    Created buy and sell forms

---
+++ src/components/SellForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import tokenLogo from '../token-logo.png'
+import ethLogo from '../eth-logo.png'
+
+class SellForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      output: '0'
+    }
+  }
+
+  render() {
+    return (
+        <div>Sell Form...</div>
+    );
+  }
+}
+
+export default SellForm;
